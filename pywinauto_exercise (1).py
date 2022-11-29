b17991664e235ffe5584e2b2a2281128aca01e25
Johnny Nylund <91679773+jaygoode@users.noreply.github.com>
2022-11-29 12:43:19 +0200

    added open_notepad function

--- pywinauto_exercise (1).py
+++ pywinauto_exercise (1).py
@@ -3,6 +3,9 @@
 # suggested imports (requires first installing the pywinauto library)
 import re  # optional
 import pywinauto
+from pywinauto.application import Application
+import time
+
 
 # instructions:
 # - pip install (--user) -r pywinauto
@@ -13,38 +16,48 @@ import pywinauto
 
 
 # implement your code here
+
+# app.UntitledNotepad.type_keys("%FX")
+# app.UntitledNotepad.menu_select("File->SaveAs")
+# app.SaveAs.ComboBox5.select("UTF-8")
+# app.SaveAs.edit1.set_text("Example-utf8.txt")
+# app.SaveAs.Save.click()
+
+
 def open_notepad():
     print("open notepad")
+    app = Application(backend="uia").start("notepad.exe").connect(
+        title="Untitled - Notepad", timeout=10)
 
 
-def close_notepad():
-    print("close notepad")
+# def close_notepad():
+#     print("close notepad")
 
 
-def open_about_notepad_view():
-    print("open notepad about view")
+# def open_about_notepad_view():
+#     print("open notepad about view")
 
 
-def get_number_of_headings():
-    print("get number of headings")
+# def get_number_of_headings():
+#     print("get number of headings")
 
 
-def close_all_sub_windows():
-    print("close all sub windows")
+# def close_all_sub_windows():
+#     print("close all sub windows")
 
 
-def open_microsoft_licence_link():
-    print("open microsoft license link")
+# def open_microsoft_licence_link():
+#     print("open microsoft license link")
 
 
 # so it can run this code below
 if __name__ == "__main__":
     app = open_notepad()  # basic Notpad application in Windows
-    open_about_notepad_view(app)
-    open_microsoft_licence_link(app)  # from notepad about view
-    # count how many NUMBERED headings there are in Lisense term view
-    # example of numbered heading: "13.	Consumer Rights, Regional Variations."
-    n_headings = get_number_of_headings(app)
-    close_all_sub_windows(app)
-    close_notepad(app)
-    assert n_headings == 15, f"Wrong number of headings: {n_headings}"
+    # open_about_notepad_view(app)
+    # open_microsoft_licence_link(app)  # from notepad about view
+    # # count how many NUMBERED headings there are in Lisense term view
+    # # example of numbered heading: "13.	Consumer Rights, Regional Variations."
+    # n_headings = get_number_of_headings(app)
+    # close_all_sub_windows(app)
+    # close_notepad(app)
+    # assert n_headings == 15, f"Wrong number of headings: {n_headings}"
